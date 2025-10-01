bca636cdbfe6657df8c4f95d3ecd7dfdf9466a56
DanielEscobar11 <drei100505@gmail.com>
2025-10-01 10:21:19 -0600

    Cambios, inicio y ubicacion

--- pruevaproyecto/src/app/pages/inicio/inicio.page.ts
+++ pruevaproyecto/src/app/pages/inicio/inicio.page.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule, RouterModule]
 })
-export class InicioPage {}
+export class InicioPage {
+  // Configuración del carrusel
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+    },
+    loop: true,
+    spaceBetween: 10,
+    centeredSlides: true
+  };
+}
